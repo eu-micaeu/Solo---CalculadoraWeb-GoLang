aae1727ae5939c8d7749529299b4039b0d2a64c5
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-02-27 12:12:12 -0300

    by Micael

---
+++ main.go
@@ -0,0 +1,27 @@
+﻿package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		display := r.FormValue("display")
+		result, err := strconv.ParseFloat(display, 64)
+		if err != nil {
+			http.Error(w, "Erro ao converter o valor", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "%.2f", result)
+	} else {
+		http.ServeFile(w, r, "index.html")
+	}
+}
